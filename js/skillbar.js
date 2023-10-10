3fe3bdc614148eaee26704244ecef8849db5cfe2
Mani Sankae <manisankar318.ms@gmail.com>
2023-10-10 16:21:28 +0530

    new changes

---
+++ js/skillbar.js
@@ -0,0 +1,16 @@
+var php = function(){
+  document.querySelector(".php").className = "php1";
+};
+var css = function(){
+  document.querySelector(".css").className = "css1";
+};
+var htm = function(){
+document.querySelector(".htm").className = "htm1";  
+};
+var jav = function(){
+document.querySelector(".javascript").className = "javascript1";  
+};
+setTimeout(php,500);
+setTimeout(css,600);
+setTimeout(htm,700);
+setTimeout(jav,800);
